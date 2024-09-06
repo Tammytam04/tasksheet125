5e1c6330389128e687a1d19b242920d0b5bf7402
John Paul Ebid <120429052+Tammytam04@users.noreply.github.com>
2024-09-06 22:19:26 +0800

    Update Tasksheet125.java

--- Tasksheet125.java
+++ Tasksheet125.java
@@ -1,28 +1,21 @@
-// Base class Vehicle
 class Vehicle {
-
     String make;
     String model;
     int year;
-
     public Vehicle(String make, String model, int year) {
         this.make = make;
         this.model = model;
         this.year = year;
     }
 }
-
-
 class Car extends Vehicle {
     int numberOfDoors;
 
-   
     public Car(String make, String model, int year, int numberOfDoors) {
      
         super(make, model, year);
         this.numberOfDoors = numberOfDoors;
     }
-
     public void displayDetails() {
         System.out.println("Car Details:");
         System.out.println("Make: " + make);
@@ -31,7 +24,6 @@ class Car extends Vehicle {
         System.out.println("Number of Doors: " + numberOfDoors);
     }
 }
-
 
 public class Tasksheet125 {
     public static void main(String[] args) {
